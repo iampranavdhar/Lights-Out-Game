0538a2e01ed9a8e7d70d7edd23103a1f67207240
N.Sai Pranavdhar Reddy <73348574+iampranavdhar@users.noreply.github.com>
2021-04-04 00:01:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import Board from "./Board";
 import "./App.css";
 
-/** Simple app that just shows the LightsOut game. */
-
-// Also you might wanna encapsulate <App /> with ApolloProvider to allow for any query anywhere in this application
-// uri should be localhost in this case
-
 function App() {
     return (
       <div className="App">
